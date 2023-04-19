9952b8361559902852f0f71bc7f6517c527e352a
Muhammad Rizqi Ardiansyah <muhrizqiardi@gmail.com>
2023-04-19 21:57:30 +0700

    feat: change to snake_case

--- server/db/migrations/20230419141307_init.sql
+++ server/db/migrations/20230419141307_init.sql
@@ -1,73 +1,73 @@
 -- migrate:up
 create extension if not exists "uuid-ossp";
 
-create table if not exists public."User" (
+create table if not exists public.users (
     id uuid default uuid_generate_v4() primary key, 
     email text not null unique,
-    fullName text not null,
+    full_name text not null,
     password text not null
 );
 
-create table if not exists public."Folder" (
+create table if not exists public.folders (
     id uuid default uuid_generate_v4() primary key,
     name text not null unique,
-    ownerId uuid not null,
+    owner_id uuid not null,
 
-    foreign key (ownerId)
-        references public."User" (id)
+    foreign key (owner_id)
+        references public.users (id)
 );
 
-create table if not exists public."Tag" (
+create table if not exists public.tags (
     id uuid default uuid_generate_v4() primary key,
     name text not null unique,
-    ownerId uuid not null,
+    owner_id uuid not null,
 
-    foreign key (ownerId)
-        references public."User" (id)
+    foreign key (owner_id)
+        references public.users (id)
 );
 
-create table if not exists public."Link" (
+create table if not exists public.links (
     id uuid default uuid_generate_v4() primary key,
     url text not null unique,
     excerpt text not null,
-    coverUrl text not null,
-    ownerId uuid not null,
-    folderId uuid not null,
+    cover_url text not null,
+    owner_id uuid not null,
+    folder_id uuid not null,
 
-    foreign key (ownerId)
-        references public."User" (id),
-    foreign key (folderId)
-        references public."Folder" (id)
+    foreign key (owner_id)
+        references public.users (id),
+    foreign key (folder_id)
+        references public.folders (id)
 );
 
-create table if not exists public."LinkMedia" (
+create table if not exists public.link_medias (
     id uuid default uuid_generate_v4() primary key,
-    linkId uuid not null,
+    link_id uuid not null,
     mediaUrl text not null,
-    ownerId uuid not null,
+    owner_id uuid not null,
 
-    foreign key (ownerId)
-        references public."User" (id),
-    foreign key (linkId)
-        references public."Link" (id)
+    foreign key (owner_id)
+        references public.users (id),
+    foreign key (link_id)
+        references public.links (id)
 );
 
-create table if not exists public."File" (
+create table if not exists public.files (
     id uuid default uuid_generate_v4() primary key,
-    linkId uuid not null,
-    fileUrl text not null,
-    ownerId uuid not null,
+    link_id uuid not null,
+    file_url text not null,
+    owner_id uuid not null,
 
-    foreign key (ownerId)
-        references public."User" (id),
-    foreign key (linkId)
-        references public."Link" (id)
+    foreign key (owner_id)
+        references public.users (id),
+    foreign key (link_id)
+        references public.links (id)
 );
 
 -- migrate:down
-drop table public."File";
-drop table public."LinkMedia";
-drop table public."Link";
-drop table public."Folder";
-drop table public."Tag";
-drop table public."User";
+drop table public.files;
+drop table public.link_medias;
+drop table public.links;
+drop table public.folders;
+drop table public.tags;
+drop table public.users;
