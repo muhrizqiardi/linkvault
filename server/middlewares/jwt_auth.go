0aba96269cc622530359f3b77693fe25ed59b34b
Muhammad Rizqi Ardiansyah <muhrizqiardi@gmail.com>
2023-04-24 18:23:49 +0700

    feat: add middleware for authentication

---
+++ server/middlewares/jwt_auth.go
@@ -0,0 +1,48 @@
+package middlewares
+
+import (
+	"context"
+	"database/sql"
+	"log"
+	"net/http"
+	"os"
+	"server/handlers"
+	"strings"
+
+	"github.com/golang-jwt/jwt/v4"
+)
+
+func JwtAuth(ctx context.Context, l *log.Logger, pg *sql.DB) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			authHeader := r.Header.Get("Authorization")
+			if authHeader == "" {
+				http.Error(w, "Unauthorized", http.StatusUnauthorized)
+				return
+			}
+
+			token := strings.Split(authHeader, " ")
+			if len(token) == 1 {
+				http.Error(w, "Unauthorized", http.StatusUnauthorized)
+				return
+			}
+
+			parsedToken, err := jwt.ParseWithClaims(token[1], &handlers.Claims{}, func(token *jwt.Token) (interface{}, error) {
+				return []byte(os.Getenv("JWT_SECRET")), nil
+			})
+			if err != nil {
+				http.Error(w, "Unauthorized", http.StatusUnauthorized)
+				return
+			}
+
+			claims, ok := parsedToken.Claims.(*handlers.Claims)
+			if !ok && !parsedToken.Valid {
+				http.Error(w, "Unauthorized", http.StatusUnauthorized)
+				return
+			}
+
+			nextCtx := context.WithValue(r.Context(), "user", claims)
+			next.ServeHTTP(w, r.WithContext(nextCtx))
+		})
+	}
+}
