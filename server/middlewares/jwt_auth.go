be2e243787560601d56b6c087468b797d0964ec7
Muhammad Rizqi Ardiansyah <muhrizqiardi@gmail.com>
2023-04-27 16:29:16 +0700

    feat: implement better error message on `JwtMiddleware`

--- server/middlewares/jwt_auth.go
+++ server/middlewares/jwt_auth.go
@@ -2,10 +2,12 @@ package middlewares
 
 import (
 	"context"
+	"encoding/json"
 	"log"
 	"net/http"
 	"os"
 	"server/handlers"
+	"server/utils"
 	"strings"
 
 	"github.com/golang-jwt/jwt/v4"
@@ -17,28 +19,33 @@ func JwtAuth(ctx context.Context, l *log.Logger, pg *sqlx.DB) func(http.Handler)
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			authHeader := r.Header.Get("Authorization")
 			if authHeader == "" {
-				http.Error(w, "Unauthorized", http.StatusUnauthorized)
+				w.WriteHeader(http.StatusUnauthorized)
+				json.NewEncoder(w).Encode(utils.CreateBaseResponse[any](false, "Unauthorized", nil))
+				l.Println("Unauthorized request: empty Authorization header")
 				return
 			}
 
 			token := strings.Split(authHeader, " ")
 			if len(token) == 1 {
-				http.Error(w, "Unauthorized", http.StatusUnauthorized)
-				return
+				w.WriteHeader(http.StatusUnauthorized)
+				json.NewEncoder(w).Encode(utils.CreateBaseResponse[any](false, "Unauthorized", nil))
+				l.Println("Unauthorized request: invalid Authorization header")
 			}
 
 			parsedToken, err := jwt.ParseWithClaims(token[1], &handlers.Claims{}, func(token *jwt.Token) (interface{}, error) {
 				return []byte(os.Getenv("JWT_SECRET")), nil
 			})
 			if err != nil {
-				http.Error(w, "Unauthorized", http.StatusUnauthorized)
-				return
+				w.WriteHeader(http.StatusUnauthorized)
+				json.NewEncoder(w).Encode(utils.CreateBaseResponse[any](false, "Unauthorized", nil))
+				l.Println(err.Error())
 			}
 
 			claims, ok := parsedToken.Claims.(*handlers.Claims)
 			if !ok && !parsedToken.Valid {
-				http.Error(w, "Unauthorized", http.StatusUnauthorized)
-				return
+				w.WriteHeader(http.StatusUnauthorized)
+				json.NewEncoder(w).Encode(utils.CreateBaseResponse[any](false, "Unauthorized", nil))
+				l.Println("Failed to assert type")
 			}
 
 			nextCtx := context.WithValue(r.Context(), "user", claims)
