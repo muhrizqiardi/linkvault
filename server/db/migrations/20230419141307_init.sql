a52e89c74f95f9fb964a1c9bb87ce98083c34097
Muhammad Rizqi Ardiansyah <muhrizqiardi@gmail.com>
2023-04-27 10:29:53 +0700

    feat: update DB schema

--- server/db/migrations/20230419141307_init.sql
+++ server/db/migrations/20230419141307_init.sql
@@ -5,22 +5,17 @@ create table if not exists public.users (
     id uuid default uuid_generate_v4() primary key, 
     email text not null unique,
     full_name text not null,
-    password text not null
+    password text not null,
+    created_at timestamp default current_timestamp,
+    updated_at timestamp default current_timestamp
 );
 
 create table if not exists public.folders (
     id uuid default uuid_generate_v4() primary key,
     name text not null unique,
     owner_id uuid not null,
-
-    foreign key (owner_id)
-        references public.users (id)
-);
-
-create table if not exists public.tags (
-    id uuid default uuid_generate_v4() primary key,
-    name text not null unique,
-    owner_id uuid not null,
+    created_at timestamp default current_timestamp,
+    updated_at timestamp default current_timestamp,
 
     foreign key (owner_id)
         references public.users (id)
@@ -33,6 +28,8 @@ create table if not exists public.links (
     cover_url text not null,
     owner_id uuid not null,
     folder_id uuid not null,
+    created_at timestamp default current_timestamp,
+    updated_at timestamp default current_timestamp,
 
     foreign key (owner_id)
         references public.users (id),
@@ -40,11 +37,27 @@ create table if not exists public.links (
         references public.folders (id)
 );
 
+create table if not exists public.tags (
+    id uuid default uuid_generate_v4() primary key,
+    name text not null unique,
+    link_id uuid not null,
+    owner_id uuid not null,
+    created_at timestamp default current_timestamp,
+    updated_at timestamp default current_timestamp,
+
+    foreign key (owner_id)
+        references public.users (id),
+    foreign key (link_id)
+        references public.links (id)
+);
+
 create table if not exists public.link_medias (
     id uuid default uuid_generate_v4() primary key,
     link_id uuid not null,
     media_url text not null,
     owner_id uuid not null,
+    created_at timestamp default current_timestamp,
+    updated_at timestamp default current_timestamp,
 
     foreign key (owner_id)
         references public.users (id),
@@ -57,6 +70,8 @@ create table if not exists public.files (
     link_id uuid not null,
     file_url text not null,
     owner_id uuid not null,
+    created_at timestamp default current_timestamp,
+    updated_at timestamp default current_timestamp,
 
     foreign key (owner_id)
         references public.users (id),
