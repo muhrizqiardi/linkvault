dd75a768401c2748674d93bc3b00b8eb16d2c669
Muhammad Rizqi Ardiansyah <muhrizqiardi@gmail.com>
2023-04-28 10:15:05 +0700

    docs: add format struct tag

--- server/dtos/link_dto.go
+++ server/dtos/link_dto.go
@@ -1,12 +1,12 @@
 package dtos
 
 type CreateLinkDto struct {
-	Url      string `json:"url" db:"url" validate:"required,url"`
+	Url      string `json:"url" db:"url" validate:"required,url" format:"url"`
 	Excerpt  string `json:"excerpt" db:"excerpt" validate:"required,string"`
-	CoverUrl string `json:"cover_url" db:"cover_url" validate:"required,url"`
+	CoverUrl string `json:"cover_url" db:"cover_url" validate:"required,url" format:"url"`
 }
 
 type UpdateLinkDto struct {
-	Excerpt  string `json:"excerpt,omitempty" db:"excerpt" validate:"required,string"`
-	CoverUrl string `json:"cover_url,omitempty" db:"cover_url" validate:"required,url"`
+	Excerpt  string `json:"excerpt,omitempty" db:"excerpt" validate:"required,string" format:"url"`
+	CoverUrl string `json:"cover_url,omitempty" db:"cover_url" validate:"required,url" format:"url"`
 }
