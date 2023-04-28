7947f75778bfc951b0ffae006cf48fd9f7942bff
Muhammad Rizqi Ardiansyah <muhrizqiardi@gmail.com>
2023-04-28 09:56:15 +0700

    feat: add handler `CreateLink`

--- server/handlers/link_handler.go
+++ server/handlers/link_handler.go
@@ -2,46 +2,98 @@ package handlers
 
 import (
 	"context"
-	"database/sql"
+	"encoding/json"
 	"log"
 	"net/http"
+	"server/dtos"
+	"server/services"
+	"server/utils"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/google/uuid"
+	"github.com/jmoiron/sqlx"
 )
 
 type LinkHandler struct {
-	ctx context.Context
-	l   *log.Logger
-	pg  *sql.DB
+	ctx         context.Context
+	l           *log.Logger
+	linkService services.LinkService
 }
 
-func NewLinkHandler(ctx context.Context, l *log.Logger, pg *sql.DB) *LinkHandler {
+func NewLinkHandler(ctx context.Context, l *log.Logger, pg *sqlx.DB) *LinkHandler {
+	linkService := services.NewLinkService(ctx, l, pg)
 	return &LinkHandler{
 		ctx,
 		l,
-		pg,
+		*linkService,
 	}
 }
 
+// Create link on default folder
+//
+// @Summary Create link
+// @Tags link
+// @Accept json
+// @Procedure json
+// @Param userId path uuid true "User/Owner ID"
+// @Success 201 {object} utils.BaseResponse[entities.LinkEntity]
+// @Failure 4xx {object} utils.BaseResponse[any]
+// @Failure 5xx {object} utils.BaseResponse[any]
+// @Router /users/{userId}/links
 func (l *LinkHandler) CreateLink(w http.ResponseWriter, r *http.Request) {
-	// createLinkQuery := `
-	// 	insert into public.links (url, excerpt, cover_url, owner_id, folder_id)
-	//    		values ($1, $2, $3, $4, $5)
-	//    		returning id, url, excerpt, cover_url, owner_id, folder_id;
-	// `
-	// w.Header().Set("Content-Type", "application/json")
+	userId, parseUserIdErr := uuid.Parse(chi.URLParam(r, "userId"))
+	if parseUserIdErr != nil {
+		utils.BaseResponseWriter[any](
+			w,
+			http.StatusBadRequest,
+			false,
+			"Bad Request",
+			nil,
+		)
+		return
+	}
+	folderId, parseFolderIdErr := uuid.Parse(chi.URLParam(r, "folderId"))
+	if parseFolderIdErr != nil {
+		utils.BaseResponseWriter[any](
+			w,
+			http.StatusBadRequest,
+			false,
+			"Bad Request",
+			nil,
+		)
+		return
+	}
 
-	// _ := chi.URLParam(r, "userId")
-	// var newLinkParams dtos.CreateLinkDto
-	// if decErr := json.NewDecoder(r.Body).Decode(&newLinkParams); decErr != nil {
-	// 	w.WriteHeader(http.StatusBadRequest)
-	// 	json.NewEncoder(w).Encode(utils.CreateBaseResponse[any](false, "Bad request", nil))
-	// 	return
-	// }
+	var payload dtos.CreateLinkDto
+	if err := json.NewDecoder(r.Body).Decode(&payload); err != nil {
+		utils.BaseResponseWriter[any](
+			w,
+			http.StatusBadRequest,
+			false,
+			"Bad Request",
+			nil,
+		)
+		return
+	}
 
-	// var queryResult dtos.CreateLinkDto
-	// if err := l.pg.QueryRowContext(l.ctx, createLinkQuery, newLinkParams.Url, newLinkParams.Excerpt, newLinkParams).Scan(&queryResult); err != nil {
-	// 	w.WriteHeader(http.StatusInternalServerError)
-	// 	json.NewEncoder(w).Encode(utils.CreateBaseResponse[any](false, "Failed to create link", nil))
-	// 	return
-	// }
+	newLink, createErr := l.linkService.Create(userId, folderId, payload)
+	if createErr != nil {
+		utils.BaseResponseWriter[any](
+			w,
+			http.StatusBadRequest,
+			false,
+			"Bad Request",
+			nil,
+		)
+		return
+	}
 
+	utils.BaseResponseWriter(
+		w,
+		http.StatusCreated,
+		true,
+		"Successfully created link",
+		newLink,
+	)
+	return
 }
