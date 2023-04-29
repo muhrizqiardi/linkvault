615e9322266caa0f1872dca2e04df2c4af3cc2d2
Muhammad Rizqi Ardiansyah <muhrizqiardi@gmail.com>
2023-04-29 22:36:18 +0700

    feat: use `utils.BaseResponseWriter` to return response

--- server/middlewares/jwt_auth.go
+++ server/middlewares/jwt_auth.go
@@ -2,7 +2,6 @@ package middlewares
 
 import (
 	"context"
-	"encoding/json"
 	"log"
 	"net/http"
 	"os"
@@ -19,16 +18,14 @@ func JwtAuth(ctx context.Context, l *log.Logger, pg *sqlx.DB) func(http.Handler)
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			authHeader := r.Header.Get("Authorization")
 			if authHeader == "" {
-				w.WriteHeader(http.StatusUnauthorized)
-				json.NewEncoder(w).Encode(utils.CreateBaseResponse[any](false, "Unauthorized", nil))
+				utils.BaseResponseWriter[any](w, http.StatusUnauthorized, false, "Unauthorized", nil)
 				l.Println("Unauthorized request: empty Authorization header")
 				return
 			}
 
 			token := strings.Split(authHeader, " ")
 			if len(token) == 1 {
-				w.WriteHeader(http.StatusUnauthorized)
-				json.NewEncoder(w).Encode(utils.CreateBaseResponse[any](false, "Unauthorized", nil))
+				utils.BaseResponseWriter[any](w, http.StatusUnauthorized, false, "Unauthorized", nil)
 				l.Println("Unauthorized request: invalid Authorization header")
 			}
 
@@ -36,15 +33,13 @@ func JwtAuth(ctx context.Context, l *log.Logger, pg *sqlx.DB) func(http.Handler)
 				return []byte(os.Getenv("JWT_SECRET")), nil
 			})
 			if err != nil {
-				w.WriteHeader(http.StatusUnauthorized)
-				json.NewEncoder(w).Encode(utils.CreateBaseResponse[any](false, "Unauthorized", nil))
+				utils.BaseResponseWriter[any](w, http.StatusUnauthorized, false, "Unauthorized", nil)
 				l.Println(err.Error())
 			}
 
 			claims, ok := parsedToken.Claims.(*handlers.Claims)
 			if !ok && !parsedToken.Valid {
-				w.WriteHeader(http.StatusUnauthorized)
-				json.NewEncoder(w).Encode(utils.CreateBaseResponse[any](false, "Unauthorized", nil))
+				utils.BaseResponseWriter[any](w, http.StatusUnauthorized, false, "Unauthorized", nil)
 				l.Println("Failed to assert type")
 			}
 
