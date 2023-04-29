4a31b968eb50bf9b804a5ec47580721de645adc4
Muhammad Rizqi Ardiansyah <muhrizqiardi@gmail.com>
2023-04-29 23:26:31 +0700

    fix: fix struct tags for a DTO

--- server/dtos/link_dto.go
+++ server/dtos/link_dto.go
@@ -8,7 +8,7 @@ type CreateLinkDto struct {
 }
 
 type UpdateLinkDto struct {
-	Title    string `json:"title" db:"title" validation:"required"`
-	Excerpt  string `json:"excerpt,omitempty" db:"excerpt" validate:"required,string" format:"url"`
+	Title    string `json:"title,omitempty" db:"title" validation:"required"`
+	Excerpt  string `json:"excerpt,omitempty" db:"excerpt" validate:"required" format:"url"`
 	CoverUrl string `json:"cover_url,omitempty" db:"cover_url" validate:"required,url" format:"url"`
 }
