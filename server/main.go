3a751c0e233be90021a56c5118475e8add0453b8
Muhammad Rizqi Ardiansyah <muhrizqiardi@gmail.com>
2023-04-29 22:51:37 +0700

    feat: add route for get many links inside a foler

--- server/main.go
+++ server/main.go
@@ -80,6 +80,7 @@ func main() {
 
 		r.Post("/folders/{folderId}/links", linkHanlder.CreateLink)
 		r.Get("/links", linkHanlder.GetManyLinks)
+		r.Get("/folders/{folderId}/links", linkHanlder.GetManyLinksInFolder)
 	})
 	http.ListenAndServe(":9000", r)
 }
