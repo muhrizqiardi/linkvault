53c63dc547363dfa8e881000e9cb297b23f4d8ec
Muhammad Rizqi Ardiansyah <muhrizqiardi@gmail.com>
2023-04-29 22:32:34 +0700

    feat: add `Title` field

--- server/dtos/link_dto.go
+++ server/dtos/link_dto.go
@@ -2,11 +2,13 @@ package dtos
 
 type CreateLinkDto struct {
 	Url      string `json:"url" db:"url" validate:"required,url" format:"url"`
-	Excerpt  string `json:"excerpt" db:"excerpt" validate:"required,string"`
+	Title    string `json:"title" db:"title" validation:"required"`
+	Excerpt  string `json:"excerpt" db:"excerpt" validate:"required"`
 	CoverUrl string `json:"cover_url" db:"cover_url" validate:"required,url" format:"url"`
 }
 
 type UpdateLinkDto struct {
+	Title    string `json:"title" db:"title" validation:"required"`
 	Excerpt  string `json:"excerpt,omitempty" db:"excerpt" validate:"required,string" format:"url"`
 	CoverUrl string `json:"cover_url,omitempty" db:"cover_url" validate:"required,url" format:"url"`
 }
