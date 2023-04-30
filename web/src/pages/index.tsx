76ae6db14de28538ea3a8d7962efb2267628afd5
Muhammad Rizqi Ardiansyah <muhrizqiardi@gmail.com>
2023-04-30 11:19:44 +0700

    feat: create initial layout

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,6 +1,18 @@
-import Link from 'next/link';
-import { HiBars3, HiMagnifyingGlass, HiPlus } from 'react-icons/hi2';
+import { SidebarBase } from '@/components/sidebar-base';
+import { TopNavBase } from '@/components/top-nav-base';
 
 export default function Home() {
-  return <></>;
+  return (
+    <>
+      <div className="grid grid-cols-[256px_576px_minmax(0,1fr)]">
+        <div className="">
+          <SidebarBase className="border-r" />
+        </div>
+        <div className="border-r">
+          <TopNavBase />
+        </div>
+        <div className=""></div>
+      </div>
+    </>
+  );
 }
