0a3f036d8ba9a3498075bd08af1edc8ebdda601f
Muhammad Rizqi Ardiansyah <muhrizqiardi@gmail.com>
2023-04-30 12:08:58 +0700

    feat: add stories for `Card`

---
+++ web/src/components/ui/card.stories.tsx
@@ -0,0 +1,41 @@
+import { HiFolder } from 'react-icons/hi2';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from './card';
+
+export const CardStory = () => (
+  <Card>
+    <div className="flex">
+      <div className="p-3.5 pr-0">
+        <img src="https://picsum.photos/id/237/200/300" alt="" className="aspect-[1200/628] w-full object-contain bg-neutral-200 rounded-sm" />
+      </div>
+
+      <div className="block">
+        <CardHeader>
+          <CardDescription className="inline-flex items-center">
+            twitter.com · 2h ago ·{' '}
+            <span className="ml-2 inline-flex items-center">
+              <HiFolder className="mr-1" />
+              Bookmarked Tweets
+            </span>
+          </CardDescription>
+          <CardTitle>Kabar Penumpang on Twitter</CardTitle>
+        </CardHeader>
+        <CardContent className="text-sm">
+          <p>
+            Menolak Lupa Proposal Kereta Cepat Jakarta Bandung Jepang: US$6,2
+            miliar (75 persennya ditanggung Jepang berupa pinjaman tenor 40 thn
+            dan bunga 0,1 persen). Menurut kajian, proyek kereta cepat sulit dgn
+            skema b to b atau tanpa jaminan pemerintah. China pun dtg skema
+            menggiurkan
+          </p>
+        </CardContent>
+      </div>
+    </div>
+  </Card>
+);
