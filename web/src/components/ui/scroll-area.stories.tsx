d307f301a88aeed55cedafb252ab8b56fd883bdc
Muhammad Rizqi Ardiansyah <muhrizqiardi@gmail.com>
2023-04-30 11:19:13 +0700

    feat: add stories for `ScrollArea`

---
+++ web/src/components/ui/scroll-area.stories.tsx
@@ -0,0 +1,41 @@
+import { ScrollArea } from './scroll-area';
+
+export const ScrollAreaStory = () => (
+  <>
+    <ScrollArea className="h-[200px] w-[350px] rounded-md border p-4">
+      <p>… The rusted chains of prison moons</p>
+      <p>Are shattered by the sun</p>
+      <p>I walk a road, horizons change</p>
+      <p>The tournament's begun</p>
+      <p>The purple piper plays his tune</p>
+      <p>The choir softly sing</p>
+      <p>Three lullabies in an ancient tongue</p>
+      <p>For the court of the crimson king</p>
+      <p>… The keeper of the city keys</p>
+      <p>Put shutters on the dreams</p>
+      <p>I wait outside the pilgrim's door</p>
+      <p>With insufficient schemes</p>
+      <p>The black queen chants</p>
+      <p>The funeral march</p>
+      <p>The cracked brass bells will ring</p>
+      <p>To summon back the fire witch</p>
+      <p>To the court of the crimson king</p>
+      <p>… The gardener plants an evergreen</p>
+      <p>Whilst trampling on a flower</p>
+      <p>I chase the wind of a prism ship</p>
+      <p>To taste the sweet and sour</p>
+      <p>The pattern juggler lifts his hand</p>
+      <p>The orchestra begin</p>
+      <p>As slowly turns the grinding wheel</p>
+      <p>In the court of the crimson king</p>
+      <p>… On soft gray mornings widows cry</p>
+      <p>The wise men share a joke</p>
+      <p>I run to grasp divining signs</p>
+      <p>To satisfy the hoax</p>
+      <p>The yellow jester does not play</p>
+      <p>But gentle pulls the strings</p>
+      <p>And smiles as the puppets dance</p>
+      <p>In the court of the crimson king</p>
+    </ScrollArea>
+  </>
+);
