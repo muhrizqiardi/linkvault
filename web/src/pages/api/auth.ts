c7811ab2ee77523981c5c7593345b04ff818d938
Muhammad Rizqi Ardiansyah <muhrizqiardi@gmail.com>
2023-05-03 15:40:31 +0700

    feat: add other methods for /api/auth endpoints

--- web/src/pages/api/auth.ts
+++ web/src/pages/api/auth.ts
@@ -20,11 +20,53 @@ export default async function handler(
         const responseBody = await response.json();
         return res
           .status(201)
-          .setHeader('Set-Cookie', `token=${responseBody.data}`)
+          .setHeader(
+            'Set-Cookie',
+            `token=${responseBody.data}; expires=${
+              new Date().getTime() + 1000 * 60 * 60 * 24 * 30 * 6
+            }`,
+          )
           .send({
             success: true,
             message: 'Successfully signed in',
           });
-      } catch (error) {}
+      } catch (error) {
+        return res
+          .status(401)
+          .send({ success: false, message: 'Failed to sign in' });
+      }
+    case 'GET':
+      try {
+        if (!req.cookies['token']) throw new Error('Unauthorized');
+        // TODO: fetch GET /auth to check the validity of the token received
+        return res
+          .status(200)
+          .setHeader(
+            'Set-Cookie',
+            `token=${req.cookies['token']}; expires=${
+              new Date().getTime() + 1000 * 60 * 60 * 24 * 30 * 6
+            }`,
+          );
+      } catch (error) {
+        return res
+          .status(401)
+          .setHeader(
+            'Set-Cookie',
+            'token=deleted; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT',
+          );
+      }
+    case 'DELETE':
+      return res
+        .status(401)
+        .setHeader(
+          'Set-Cookie',
+          'token=deleted; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT',
+        );
+
+    default:
+      return res.status(200).send({
+        success: true,
+        message: 'Not Found',
+      });
   }
 }
