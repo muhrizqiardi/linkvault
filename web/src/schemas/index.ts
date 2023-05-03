ef37248c5d2d96d1fd7cbc6c95e8580fff59719d
Muhammad Rizqi Ardiansyah <muhrizqiardi@gmail.com>
2023-05-03 15:09:11 +0700

    fix: remove refine method on schema

--- web/src/schemas/index.ts
+++ web/src/schemas/index.ts
@@ -1,15 +1,11 @@
 import { z } from 'zod';
 
-export const registerPageFormDtoSchema = z
-  .object({
-    full_name: z.string(),
-    email: z.string().email(),
-    password: z.string().length(8),
-    confirm_password: z.string().length(8),
-  })
-  .refine((arg) => arg.password !== arg.confirm_password, {
-    message: 'Passwords do not match',
-  });
+export const registerPageFormDtoSchema = z.object({
+  full_name: z.string(),
+  email: z.string().email(),
+  password: z.string().length(8),
+  confirm_password: z.string().length(8),
+});
 
 export interface RegisterPageFormDto
   extends z.infer<typeof registerPageFormDtoSchema> {}
