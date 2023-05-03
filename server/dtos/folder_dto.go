d52e8c0df7256c7d1fc9f42dacda1d3424b67909
Muhammad Rizqi Ardiansyah <muhrizqiardi@gmail.com>
2023-05-04 06:11:41 +0700

    chore: remove `OwnerId` field

--- server/dtos/folder_dto.go
+++ server/dtos/folder_dto.go
@@ -1,12 +1,9 @@
 package dtos
 
-import (
-	"github.com/google/uuid"
-)
+import "github.com/google/uuid"
 
 type CreateFolderDto struct {
-	Name    string    `json:"name" db:"name" validation:"required,string"`
-	OwnerId uuid.UUID `json:"owner_id" db:"owner_id" validation:"required,uuid" format:"uuid"`
+	Name string `json:"name" db:"name" validation:"required,string"`
 }
 
 type UpdateFolderDto struct {
