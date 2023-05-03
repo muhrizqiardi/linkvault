eecd7389d7ca4beec1dba5f7a878a6149cf9bfdb
Muhammad Rizqi Ardiansyah <muhrizqiardi@gmail.com>
2023-05-03 14:13:35 +0700

    feat: move form to it's own component

--- web/src/pages/register.tsx
+++ web/src/pages/register.tsx
@@ -1,14 +1,11 @@
-import { Button } from '@/components/ui/button';
+import { RegisterPageForm } from '@/components/register-page-form';
 import {
   Card,
   CardContent,
   CardDescription,
-  CardFooter,
   CardHeader,
   CardTitle,
 } from '@/components/ui/card';
-import { Input } from '@/components/ui/input';
-import { Label } from '@/components/ui/label';
 import Link from 'next/link';
 
 export default function SignInPage() {
@@ -29,38 +26,8 @@ export default function SignInPage() {
           </CardDescription>
         </CardHeader>
         <CardContent>
-          <form>
-            <div className="grid w-full items-center gap-4">
-              <div className="flex flex-col space-y-1.5">
-                <Label htmlFor="name">Full name</Label>
-                <Input id="name" type="email" placeholder="Doe John" />
-              </div>
-              <div className="flex flex-col space-y-1.5">
-                <Label htmlFor="name">Email</Label>
-                <Input id="name" type="email" placeholder="user@example.com" />
-              </div>
-              <div className="flex flex-col space-y-1.5">
-                <Label htmlFor="name">Password</Label>
-                <Input
-                  id="name"
-                  type="password"
-                  placeholder="Enter your password here"
-                />
-              </div>
-              <div className="flex flex-col space-y-1.5">
-                <Label htmlFor="name">Password</Label>
-                <Input
-                  id="name"
-                  type="password"
-                  placeholder="Confirm your password"
-                />
-              </div>
-            </div>
-          </form>
+          <RegisterPageForm />
         </CardContent>
-        <CardFooter className="flex justify-end">
-          <Button>Create account</Button>
-        </CardFooter>
       </Card>
     </div>
   );
