0575a5a285f96c2315616b5b9e852c0ef0c0f87b
Muhammad Rizqi Ardiansyah <muhrizqiardi@gmail.com>
2023-05-05 05:29:56 +0700

    feat: separate folder list in to a new component `SidebarBaseFolderList`

--- web/src/components/sidebar-base.tsx
+++ web/src/components/sidebar-base.tsx
@@ -8,6 +8,7 @@ import {
   HiFolderPlus,
   HiHome,
 } from 'react-icons/hi2';
+import SidebarBaseFolderList from './sidebar-base-folder-list';
 import { Avatar, AvatarFallback } from './ui/avatar';
 import {
   DropdownMenuContent,
@@ -63,39 +64,7 @@ export function SidebarBase({ className }: BaseSidebarProps) {
           </div>
         </div>
 
-        <div className="px-4 py-2">
-          <div className="flex justify-between items-center">
-            <h2 className="px-2 text-lg font-semibold tracking-tight">
-              Folders
-            </h2>
-            <div className="flex gap-1">
-              <Button variant="outline" size="sm">
-                <HiEllipsisVertical />
-              </Button>
-              <Button variant="outline" size="sm">
-                <HiFolderPlus />
-              </Button>
-            </div>
-          </div>
-          <div className="space-y-1 py-2">
-            <Button variant="ghost" size="sm" className="w-full justify-start">
-              <HiFolder className="mr-2 h-4 w-4" />
-              Tweets
-            </Button>
-            <Button variant="ghost" size="sm" className="w-full justify-start">
-              <HiFolder className="mr-2 h-4 w-4" />
-              Posts
-            </Button>
-            <Button variant="ghost" size="sm" className="w-full justify-start">
-              <HiFolder className="mr-2 h-4 w-4" />
-              All about music
-            </Button>
-            <Button variant="ghost" size="sm" className="w-full justify-start">
-              <HiFolder className="mr-2 h-4 w-4" />
-              Entertainments
-            </Button>
-          </div>
-        </div>
+        <SidebarBaseFolderList />
 
         <div className="px-4 py-2">
           <div className="flex justify-between items-center">
