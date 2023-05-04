22a20a69cdb4ffdb82645459e3aa2149761dc64c
Muhammad Rizqi Ardiansyah <muhrizqiardi@gmail.com>
2023-05-05 06:19:17 +0700

    build: update to support /app router

--- web/next.config.js
+++ web/next.config.js
@@ -1,14 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
   reactStrictMode: true,
   images: {
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'picsum.photos',
-      }
-    ]
-  }
-}
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
