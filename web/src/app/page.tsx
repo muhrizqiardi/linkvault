c9df9653912953cdb4c14a311631934175554776
Muhammad Rizqi Ardiansyah <muhrizqiardi@gmail.com>
2023-05-05 06:21:18 +0700

    feat: migrate index page from /pages to /app router

---
+++ web/src/app/page.tsx
@@ -0,0 +1,20 @@
+import { LinkListCard } from '@/components/link-list-card';
+import { SidebarBase } from '@/components/sidebar-base';
+import { TopNavBase } from '@/components/top-nav-base';
+
+export default async function HomePage() {
+  return (
+    <div className="grid lg:grid-cols-[256px_576px_minmax(0,1fr)]">
+      <div className="">
+        <SidebarBase className="border-r hidden lg:block" />
+      </div>
+      <div className="border-r">
+        <TopNavBase />
+        <div className="flex flex-col">
+          <LinkListCard />
+        </div>
+      </div>
+      <div className=""></div>
+    </div>
+  );
+}
