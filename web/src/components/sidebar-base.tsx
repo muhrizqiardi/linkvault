40516d0a4c3a3c7493cb2711f09f07bf00d2e8e0
Muhammad Rizqi Ardiansyah <muhrizqiardi@gmail.com>
2023-05-04 19:17:01 +0700

    feat: add `SidebarBase`

--- web/src/components/sidebar-base.tsx
+++ web/src/components/sidebar-base.tsx
@@ -1,12 +1,22 @@
 import { Button } from '@/components/ui/button';
 import { cn } from '@/utils/cn';
+import { DropdownMenu } from '@radix-ui/react-dropdown-menu';
+import Link from 'next/link';
 import {
   HiEllipsisVertical,
   HiFolder,
   HiFolderPlus,
   HiHome,
-  HiUser,
 } from 'react-icons/hi2';
+import { Avatar, AvatarFallback } from './ui/avatar';
+import {
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from './ui/dropdown-menu';
 
 interface BaseSidebarProps extends React.HTMLAttributes<HTMLDivElement> {}
 
@@ -16,6 +26,32 @@ export function SidebarBase({ className }: BaseSidebarProps) {
       <div className="space-y-4 py-4">
         <div className="px-4 py-2">
           <div className="space-y-1">
+            <DropdownMenu>
+              <DropdownMenuTrigger className="w-full" asChild>
+                <Button
+                  variant="ghost"
+                  className="justify-start h-min mb-4 px-2 py-1"
+                >
+                  <Avatar className="h-7 w-7 text-xs">
+                    <AvatarFallback>ST</AvatarFallback>
+                  </Avatar>
+                  <span className="ml-2">Fulan binti Mulan</span>
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent className="w-56">
+                <DropdownMenuLabel>user@example.com</DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuGroup>
+                  <DropdownMenuItem>
+                    <span>Settings</span>
+                  </DropdownMenuItem>
+                  <DropdownMenuItem asChild>
+                    <a href="/sign-out">Sign out</a>
+                  </DropdownMenuItem>
+                </DropdownMenuGroup>
+              </DropdownMenuContent>
+            </DropdownMenu>
+
             <Button
               variant="secondary"
               size="sm"
@@ -23,10 +59,6 @@ export function SidebarBase({ className }: BaseSidebarProps) {
             >
               <HiHome className="mr-2 h-4 w-4" />
               All links
-            </Button>
-            <Button variant="ghost" size="sm" className="w-full justify-start">
-              <HiUser className="mr-2 h-4 w-4" />
-              Account
             </Button>
           </div>
         </div>
