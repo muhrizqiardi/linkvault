ec67453638ebf88f35e5940b6f86cf0fad086e45
Muhammad Rizqi Ardiansyah <muhrizqiardi@gmail.com>
2023-05-05 09:53:22 +0700

    feat: take user dropdown menu into a new component

---
+++ web/src/components/sidebar-base-user-dropdown-menu.tsx
@@ -0,0 +1,58 @@
+'use client';
+
+import { useUser } from '@/utils/use-user';
+import { Avatar, AvatarFallback } from './ui/avatar';
+import { Button } from './ui/button';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from './ui/dropdown-menu';
+import { Skeleton } from './ui/skeleton';
+
+export function SidebarBaseUserDropdownMenu() {
+  const { data, isLoading } = useUser();
+
+  const userButtonSkeleton = (
+    <div className="flex items-center gap-4 mb-4 px-2 py-1">
+      <Skeleton className="h-7 w-7 rounded-full flex-shrink-0" />
+      <Skeleton className="h-4 w-full" />
+    </div>
+  );
+
+  return isLoading || data === undefined ? (
+    userButtonSkeleton
+  ) : (
+    <DropdownMenu>
+      <DropdownMenuTrigger className="w-full" asChild>
+        <Button variant="ghost" className="justify-start h-min mb-4 px-2 py-1">
+          <Avatar className="h-7 w-7 text-xs">
+            <AvatarFallback className="uppercase">
+              {data.full_name
+                .split(' ')
+                .map((x) => x[0].toUpperCase())
+                .join('')}
+            </AvatarFallback>
+          </Avatar>
+          <span className="ml-2">{data.full_name}</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-56">
+        <DropdownMenuLabel>user@example.com</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuGroup>
+          <DropdownMenuItem>
+            <span>Settings</span>
+          </DropdownMenuItem>
+          <DropdownMenuItem asChild>
+            <a href="/sign-out">Sign out</a>
+          </DropdownMenuItem>
+        </DropdownMenuGroup>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
--- web/src/components/sidebar-base.tsx
+++ web/src/components/sidebar-base.tsx
@@ -1,79 +1,18 @@
-'use client';
-
 import { Button } from '@/components/ui/button';
 import { cn } from '@/utils/cn';
-import { useUser } from '@/utils/use-user';
-import { DropdownMenu } from '@radix-ui/react-dropdown-menu';
-import Link from 'next/link';
-import {
-  HiEllipsisVertical,
-  HiFolder,
-  HiFolderPlus,
-  HiHome,
-} from 'react-icons/hi2';
+import { HiEllipsisVertical, HiFolder, HiHome } from 'react-icons/hi2';
 import SidebarBaseFolderList from './sidebar-base-folder-list';
-import { Avatar, AvatarFallback } from './ui/avatar';
-import {
-  DropdownMenuContent,
-  DropdownMenuGroup,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from './ui/dropdown-menu';
-import { Skeleton } from './ui/skeleton';
+import { SidebarBaseUserDropdownMenu } from './sidebar-base-user-dropdown-menu';
 
-interface BaseSidebarProps extends React.HTMLAttributes<HTMLDivElement> {}
+interface SidebarBaseProps extends React.HTMLAttributes<HTMLDivElement> {}
 
-export function SidebarBase({ className }: BaseSidebarProps) {
-  const { data, isLoading } = useUser();
-
-  const userButtonSkeleton = (
-    <div className="flex items-center gap-4 mb-4 px-2 py-1">
-      <Skeleton className="h-7 w-7 rounded-full flex-shrink-0" />
-      <Skeleton className="h-4 w-full" />
-    </div>
-  );
-
+export function SidebarBase({ className }: SidebarBaseProps) {
   return (
     <aside className={cn('pb-12', className)}>
       <div className="space-y-4 py-4">
         <div className="px-4 py-2">
           <div className="space-y-1">
-            {isLoading || data === undefined ? (
-              userButtonSkeleton
-            ) : (
-              <DropdownMenu>
-                <DropdownMenuTrigger className="w-full" asChild>
-                  <Button
-                    variant="ghost"
-                    className="justify-start h-min mb-4 px-2 py-1"
-                  >
-                    <Avatar className="h-7 w-7 text-xs">
-                      <AvatarFallback className="uppercase">
-                        {data.full_name
-                          .split(' ')
-                          .map((x) => x[0].toUpperCase())
-                          .join('')}
-                      </AvatarFallback>
-                    </Avatar>
-                    <span className="ml-2">{data.full_name}</span>
-                  </Button>
-                </DropdownMenuTrigger>
-                <DropdownMenuContent className="w-56">
-                  <DropdownMenuLabel>user@example.com</DropdownMenuLabel>
-                  <DropdownMenuSeparator />
-                  <DropdownMenuGroup>
-                    <DropdownMenuItem>
-                      <span>Settings</span>
-                    </DropdownMenuItem>
-                    <DropdownMenuItem asChild>
-                      <a href="/sign-out">Sign out</a>
-                    </DropdownMenuItem>
-                  </DropdownMenuGroup>
-                </DropdownMenuContent>
-              </DropdownMenu>
-            )}
+            <SidebarBaseUserDropdownMenu />
             <Button
               variant="secondary"
               size="sm"
