ad4bb741986b24337aadf96cc4ce1204bb4a83db
Muhammad Rizqi Ardiansyah <muhrizqiardi@gmail.com>
2023-05-05 09:32:09 +0700

    feat: add user info loading

--- web/src/components/sidebar-base.tsx
+++ web/src/components/sidebar-base.tsx
@@ -2,6 +2,7 @@
 
 import { Button } from '@/components/ui/button';
 import { cn } from '@/utils/cn';
+import { useUser } from '@/utils/use-user';
 import { DropdownMenu } from '@radix-ui/react-dropdown-menu';
 import Link from 'next/link';
 import {
@@ -20,41 +21,59 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from './ui/dropdown-menu';
+import { Skeleton } from './ui/skeleton';
 
 interface BaseSidebarProps extends React.HTMLAttributes<HTMLDivElement> {}
 
 export function SidebarBase({ className }: BaseSidebarProps) {
+  const { data, isLoading } = useUser();
+
+  const userButtonSkeleton = (
+    <div className="flex items-center gap-4 mb-4 px-2 py-1">
+      <Skeleton className="h-7 w-7 rounded-full flex-shrink-0" />
+      <Skeleton className="h-4 w-full" />
+    </div>
+  );
+
   return (
     <aside className={cn('pb-12', className)}>
       <div className="space-y-4 py-4">
         <div className="px-4 py-2">
           <div className="space-y-1">
-            <DropdownMenu>
-              <DropdownMenuTrigger className="w-full" asChild>
-                <Button
-                  variant="ghost"
-                  className="justify-start h-min mb-4 px-2 py-1"
-                >
-                  <Avatar className="h-7 w-7 text-xs">
-                    <AvatarFallback>ST</AvatarFallback>
-                  </Avatar>
-                  <span className="ml-2">Fulan binti Mulan</span>
-                </Button>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent className="w-56">
-                <DropdownMenuLabel>user@example.com</DropdownMenuLabel>
-                <DropdownMenuSeparator />
-                <DropdownMenuGroup>
-                  <DropdownMenuItem>
-                    <span>Settings</span>
-                  </DropdownMenuItem>
-                  <DropdownMenuItem asChild>
-                    <a href="/sign-out">Sign out</a>
-                  </DropdownMenuItem>
-                </DropdownMenuGroup>
-              </DropdownMenuContent>
-            </DropdownMenu>
-
+            {isLoading || data === undefined ? (
+              userButtonSkeleton
+            ) : (
+              <DropdownMenu>
+                <DropdownMenuTrigger className="w-full" asChild>
+                  <Button
+                    variant="ghost"
+                    className="justify-start h-min mb-4 px-2 py-1"
+                  >
+                    <Avatar className="h-7 w-7 text-xs">
+                      <AvatarFallback className="uppercase">
+                        {data.full_name
+                          .split(' ')
+                          .map((x) => x[0].toUpperCase())
+                          .join('')}
+                      </AvatarFallback>
+                    </Avatar>
+                    <span className="ml-2">{data.full_name}</span>
+                  </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent className="w-56">
+                  <DropdownMenuLabel>user@example.com</DropdownMenuLabel>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuGroup>
+                    <DropdownMenuItem>
+                      <span>Settings</span>
+                    </DropdownMenuItem>
+                    <DropdownMenuItem asChild>
+                      <a href="/sign-out">Sign out</a>
+                    </DropdownMenuItem>
+                  </DropdownMenuGroup>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            )}
             <Button
               variant="secondary"
               size="sm"
