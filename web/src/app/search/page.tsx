9f1858f4eb5130e0844f9dc4570305cf6b19b3e4
Muhammad Rizqi Ardiansyah <muhrizqiardi@gmail.com>
2023-05-06 20:34:28 +0700

    feat: move from /pages routing to App Router

--- web/src/app/search/page.tsx
+++ web/src/app/search/page.tsx
@@ -1,6 +1,5 @@
 import { LinkListCard } from '@/components/link-list-card';
 import { SidebarBase } from '@/components/sidebar-base';
-import { TopNavBase } from '@/components/top-nav-base';
 import TopNavSearch from '@/components/top-nav-search';
 
 export default function SearchPage() {
@@ -11,7 +10,9 @@ export default function SearchPage() {
           <SidebarBase className="border-r" />
         </div>
         <div className="border-r">
-          <TopNavSearch />
+          <div className="h-14">
+            <TopNavSearch />
+          </div>
           <div className="flex flex-col">
             <LinkListCard />
           </div>
