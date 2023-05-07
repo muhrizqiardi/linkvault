bfaf5db663f92fcb86aafefcaddcaf74759dcf46
Muhammad Rizqi Ardiansyah <muhrizqiardi@gmail.com>
2023-05-07 14:34:29 +0700

    chore: remove unused parent elements

--- web/src/app/search/page.tsx
+++ web/src/app/search/page.tsx
@@ -5,19 +5,11 @@ import TopNavSearch from '@/components/top-nav-search';
 export default function SearchPage() {
   return (
     <>
-      <div className="grid grid-cols-[256px_576px_minmax(0,1fr)]">
-        <div className="">
-          <SidebarBase className="border-r" />
-        </div>
-        <div className="border-r">
-          <div className="h-14">
-            <TopNavSearch />
-          </div>
-          <div className="flex flex-col">
-            <LinkListCard />
-          </div>
-        </div>
-        <div className=""></div>
+      <div className="h-14">
+        <TopNavSearch />
+      </div>
+      <div className="flex flex-col">
+        <LinkListCard />
       </div>
     </>
   );
